af53a2bf384729e1461556bd0eac2102ed9d4c6b
frank-gear <75234380+frank-gear@users.noreply.github.com>
2021-11-10 15:51:12 +0700

    Update hello.py
    hello world complete

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,28 +1,38 @@
 #! /usr/bin/env python
 """
-Author: Frank
+Author : frank <frank@localhost>
+Date   : 2021-11-10
 Purpose: Say Hello
 """
 
 import argparse
 
 
+# --------------------------------------------------
 def get_args():
-    """Get the command-line arguments"""
+    """Get command-line arguments"""
 
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name',
-                        default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(
+        description='Say Hello',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n',
+                        '--name',
+                        help='Name to greet',
+                        metavar='name',
+                        type=str,
+                        default='World')
+
+
     return parser.parse_args()
 
 
+# --------------------------------------------------
 def main():
-    """Main"""
+    """Make a jazz noise here"""
 
     args = get_args()
-    name = args.name
-    print('Hello, ' + name + '!')
+    print('Hello, ' + args.name + '!')
 
-
+# --------------------------------------------------
 if __name__ == '__main__':
     main()
