c32b37da8fd51601433fc7f10e9d9b667a294c36
frank-gear <75234380+frank-gear@users.noreply.github.com>
2021-11-10 14:29:19 +0700

    Update hello.py
    update

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,15 +1,28 @@
 #! /usr/bin/env python
-# python -m pytest -xv test.py
+"""
+Author: Frank
+Purpose: Say Hello
+"""
+
 import argparse
+
+
 def get_args():
+    """Get the command-line arguments"""
+
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n','--name', metavar='name', default='World', help='Name to greet')
-    return  parser.parse_args()
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
 
 def main():
+    """Main"""
+
     args = get_args()
     name = args.name
     print('Hello, ' + name + '!')
+
 
 if __name__ == '__main__':
     main()
