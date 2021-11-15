fe7588165eaecf7045ad8d505d6451616512ccc2
frank-gear <75234380+frank-gear@users.noreply.github.com>
2021-11-15 22:55:44 +0700

    Update jump.py
    update

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -2,7 +2,7 @@
 """
 Author : frank <frank@localhost>
 Date   : 2021-11-11
-Purpose: Rock the Casbah
+Purpose: number changer
 """
 
 import argparse
@@ -13,38 +13,13 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='number changer',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('number',
                         metavar='str',
                         help='A positional argument')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -54,18 +29,22 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
     pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
     print(f'positional = "{pos_arg}"')
 
+    numbers = {
+        "1" : '9',
+        '2' : '8',
+        '3' : '7',
+        '4' : '6',
+        '5' : '0',
+        '6' : '4',
+        '7' : '3',
+        '8' : '2',
+        '9' : '1',
+        '0' : '5'
+    }
 
 # --------------------------------------------------
 if __name__ == '__main__':
